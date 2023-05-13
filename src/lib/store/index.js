950085c3efc0850e9cd294f9522aaba2e5d122ab
siddharth <me@siddharthjain.in>
2023-05-14 01:20:12 +0530

    form save working

---
+++ src/lib/store/index.js
@@ -0,0 +1,7 @@
+import { writable } from 'svelte/store';
+export const embed_text = writable('');
+export const embed_source = writable('');
+export const openai_key = writable('');
+export const openai_engine = writable('');
+export const supabase_url = writable('');
+export const supabase_key = writable('');
--- src/routes/Embed.svelte
+++ src/routes/Embed.svelte
@@ -1,5 +1,19 @@
 <script>
-	import Header from '$lib/Header.svelte';
+	import Header from '$lib/components/Header.svelte';
+	import { embed_source, embed_text } from '$lib/store';
+
+	function embedSubmit() {
+		console.log($embed_text);
+		console.log($embed_source);
+	}
+
+	function embedTextChange(e) {
+		embed_text.set(e.target.value);
+	}
+
+	function embedSourceChange(e) {
+		embed_source.set(e.target.value);
+	}
 </script>
 
 <Header />
@@ -7,18 +21,27 @@
 	<div class="hero-content text-center w-full h-full">
 		<div class="w-full h-full flex flex-col">
 			<h1 class="text-3xl font-bold my-4">Add Vector Embeddings to your vector database</h1>
-			<form class="form-control flex-1 flex flex-col space-y-6">
+			<form
+				on:submit|preventDefault={embedSubmit}
+				class="form-control flex-1 flex flex-col space-y-6"
+			>
 				<label class=" flex flex-col items-start">
 					<span class="font-semibold">Text</span>
 					<textarea
 						rows="12"
 						placeholder="Enter Text to Embed"
 						class="textarea rounded-none textarea-primary textarea-lg w-full"
+						on:input={embedTextChange}
 					/>
 				</label>
 				<div class="flex flex-col items-start">
 					<span class="font-semibold">Source</span>
-					<input type="text" placeholder="10" class="input input-bordered input-primary w-full" />
+					<input
+						type="text"
+						placeholder="https://www.source.com/"
+						class="input input-bordered input-primary w-full"
+						on:input={embedSourceChange}
+					/>
 					<span class="font-thin"
 						>Recommended to add Source URL so that it can be referred by LLM</span
 					>

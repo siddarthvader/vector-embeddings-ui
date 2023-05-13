2d55b247ec307fd88149fa7c76a0f2886d11e8ce
siddharth <me@siddharthjain.in>
2023-05-13 23:51:46 +0530

    layout added

--- src/routes/Embed.svelte
+++ src/routes/Embed.svelte
@@ -1,2 +1,14 @@
-<div>embedding UI on the way</div>
-<button class="btn">Generate & Save Embedding</button>
+<script>
+	import Header from '$lib/Header.svelte';
+</script>
+
+<Header />
+<div class="hero min-h-screen bg-base-200">
+	<div class="hero-content text-center">
+		<div class="max-w-md">
+			<h1 class="text-5xl font-bold">Hello there</h1>
+			<p class="py-6">UI to add Vector Embeddings to your vector database</p>
+			<button class="btn btn-primary">Get Started</button>
+		</div>
+	</div>
+</div>
